8b475ebd123a9f3e601ad4b78f8dd9fc643d4b93
Saúl Salazar <s.s.mendez@evomatik.com>
2017-09-12 09:30:59 -0500

    CHANGE - en el componente checkbox ahora puede recibir una funcion para llamarla en el checkbox

--- src/app/components/globals/partials/checkbox/checkbox.component.ts
+++ src/app/components/globals/partials/checkbox/checkbox.component.ts
@@ -8,11 +8,17 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class CheckboxComponent{
 	@Input() label : string;
 	@Input() value : string;
+	@Input() changeFunction: Function;
 
 	@Output() valueChange:EventEmitter<string> = new EventEmitter<String>()
 
 	update(value) {
 		this.valueChange.emit(value);
+	}
+
+	changeCheck(e){
+		if (this.changeFunction)
+			this.changeFunction(e);
 	}
 }
 
