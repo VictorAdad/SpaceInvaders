6fac9bf099dc93155d447563e011e46378a6272a
Ulises Hernandez <m.g.hernandez@evomatik.com>
2017-09-14 17:15:01 -0500

    Change - Breadcrumb

--- src/app/components/app/home/home.component.ts
+++ src/app/components/app/home/home.component.ts
@@ -1,11 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ActivatedRoute }    from '@angular/router';
 
 @Component({
     templateUrl: './home.component.html',
     styleUrls  :['./home.component.css']
 })
-export class HomeComponent {
-  folders = [
+export class HomeComponent implements OnInit {
+    folders = [
     {
       name    : 'CAI/AIN/UAI/268/00126/17/08',
       titulo  : 'Título de la noticia de hechos',
@@ -42,5 +43,10 @@ export class HomeComponent {
       descripcion: 'Esta es una breve y muy corta descripción',
       updated: new Date('1/28/16'),
     }
-  ];
+    ];
+    constructor(private route: ActivatedRoute) { }
+
+    ngOnInit(){
+        console.log('-> Route', this.route);
+    }
 }
