7398cf9a37eee470c19f79d6ad00499081d06546
Ulises Hernandez <m.g.hernandez@evomatik.com>
2017-09-15 17:33:18 -0500

    Change - Acuerdo General components

--- src/app/components/app/solicitud-preliminar/acuerdo-general/component.ts
+++ src/app/components/app/solicitud-preliminar/acuerdo-general/component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { MdPaginator } from '@angular/material';
 import { TableService} from '@utils/table/table.service';
 
@@ -7,43 +7,4 @@ import { TableService} from '@utils/table/table.service';
 })
 export class AcuerdoGeneralComponent {
 
-}
-
-@Component({
-	selector: 'solicitud-acuerdo-general',
-    templateUrl:'./solicitud.component.html',
-})
-export class SolicitudAcuerdoGeneralComponent {
-	displayedColumns = ['nombre', 'procedimiento', 'fechaCreacion'];
-	data: DocumentoAcuerdoGeneral[] = [
-		{id : 1, nombre: 'Entrevista.pdf',  	procedimiento: 'N/A', 		fechaCreacion:'07/09/2017'},
-		{id : 2, nombre: 'Nota.pdf',        	procedimiento: 'N/A', 		fechaCreacion:'07/09/2017'},
-		{id : 3, nombre: 'Fase.png',        	procedimiento: 'N/A', 		fechaCreacion:'07/09/2017'},
-		{id : 4, nombre: 'Entrevista1.pdf',  	procedimiento: 'N/A',     	fechaCreacion:'07/09/2017'},
-		{id : 5, nombre: 'Fase1.png',        	procedimiento: 'N/A',     	fechaCreacion:'07/09/2017'},
-	];
-
-	dataSource: TableService | null;
-	@ViewChild(MdPaginator) paginator: MdPaginator;
-
-	constructor(){}
-
-	ngOnInit() {
-    	this.dataSource = new TableService(this.paginator, this.data);
-  	}
-}
-
-@Component({
-	selector: 'documento-acuerdo-general',
-    templateUrl:'./documento.component.html',
-})
-export class DocumentoAcuerdoGeneralComponent {
-
-}
-
-export interface DocumentoAcuerdoGeneral {
-	id:number
-	nombre: string;
-	procedimiento: string;
-	fechaCreacion: string;
 }
--- src/app/components/app/solicitud-preliminar/module.ts
+++ src/app/components/app/solicitud-preliminar/module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { SolicitudAcuerdoGeneralComponent, DocumentoAcuerdoGeneralComponent } from './acuerdo-general/component';
+import { AcuerdoGeneralComponent } from './acuerdo-general/component';
+import { SolicitudAcuerdoGeneralComponent, DocumentoAcuerdoGeneralComponent } from './acuerdo-general/create/component';
 
 @NgModule()
 export class SolicitudPreliminarModule { }
 
 export const SolicitudPreliminarComponents = [
+	AcuerdoGeneralComponent,
 	SolicitudAcuerdoGeneralComponent,
     DocumentoAcuerdoGeneralComponent
 ];
