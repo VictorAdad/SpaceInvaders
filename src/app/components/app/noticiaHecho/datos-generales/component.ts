8978f4d4ed09e61f68b4d19be580714f6849a807
Saúl Salazar <s.s.mendez@evomatik.com>
2017-09-21 08:54:03 -0500

    ADD - ejemplo de indexed

--- src/app/components/app/noticiaHecho/datos-generales/component.ts
+++ src/app/components/app/noticiaHecho/datos-generales/component.ts
@@ -6,28 +6,68 @@ import { LoaderComponent } from '@partials/loader/component';
 import { MdDialog } from '@angular/material';
 
 @Component({
-	selector : 'datos-generales',
+    selector : 'datos-generales',
     templateUrl:'./component.html'
 })
 export class DatosGeneralesComponent implements OnInit{
-	public form       : FormGroup;
-	public model      : DatosGenerales;
+    public form       : FormGroup;
+    public model      : DatosGenerales;
 
-	public constructor(private _fbuilder: FormBuilder) { 
-		
-	}
+    public constructor(private _fbuilder: FormBuilder) { 
+        
+    }
 
-	ngOnInit(){
-		this.model = new DatosGenerales();
-		this.form  = new FormGroup({
-    		'titulo'   : new FormControl(this.model.titulo, [Validators.required]),
-    		'sintesis' : new FormControl(this.model.sintesis, [Validators.required]),
-    		'delito'   : new FormControl(this.model.delito, [Validators.required])
-  		});
-	}
+    ngOnInit(){
+        this.model = new DatosGenerales();
+        this.form  = new FormGroup({
+            'titulo'   : new FormControl(this.model.titulo, [Validators.required]),
+            'sintesis' : new FormControl(this.model.sintesis, [Validators.required]),
+            'delito'   : new FormControl(this.model.delito, [Validators.required])
+        });
+    }
 
-	public save(valid : any, model : any):void{
-		console.log('DatosGenerales@save()');
-	}
+    public save(valid : any, model : any):void{
+        // This works on all devices/browsers, and uses IndexedDBShim as a final fallback 
+        var indexedDB = window.indexedDB ;
+
+        // Open (or create) the database
+        //por una estraña razon no funciona los nombres si la M antes
+        var open = indexedDB.open("MEvomatik", 1);
+
+        // Create the schema
+        open.onupgradeneeded = function() {
+            var db = open.result;
+            var store = db.createObjectStore("MCasos", {keyPath: "id"});
+            var index = store.createIndex("Index", ["titulo", "delito"]);
+        };
+
+        open.onsuccess = function() {
+            // Start a new transaction
+            var db = open.result;
+            var tx = db.transaction("MCasos", "readwrite");
+            var store = tx.objectStore("MCasos");
+            var index = store.index("Index");
+
+            // Add some data
+            let json = model;
+            json.id = Date.now();
+
+            //add or update
+            store.put(json);
+
+            var list=store.getAll();
+            list.onsuccess=function(){
+                var datos=list.result;
+                console.log(datos);
+            }
+            
+            
+            // Close the db when the transaction is done
+            tx.oncomplete = function() {
+                db.close();
+            };
+        }
+        console.log('DatosGenerales@save()');
+    }
 
 }
