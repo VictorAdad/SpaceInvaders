2b5b3b70b5a8b6a90b0e2d94b27607c4ad2869b4
Saúl Salazar <s.s.mendez@evomatik.com>
2017-09-21 15:21:35 -0500

    ADD - agregado el list y el crear persona

--- src/app/components/app/noticiaHecho/persona/component.ts
+++ src/app/components/app/noticiaHecho/persona/component.ts
@@ -1,35 +1,35 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { MdPaginator } from '@angular/material';
 import { TableService} from '@utils/table/table.service';
+import { CIndexedDB } from '@services/indexedDB';
+import {Persona} from '@models/persona';
 
 @Component({
-	selector : 'persona',
+    selector : 'persona',
     templateUrl:'./component.html'
 })
 
 export class PersonaComponent implements OnInit{
-	columns = ['tipo', 'nombre', 'razonSocial', 'alias'];
-	data: Persona[];
-	dataSource: TableService | null;
+    columns = ['tipo', 'nombre', 'razonSocial', 'alias'];
+    data=[];
+    dataSource: TableService | null;
+    tabla: CIndexedDB;
     @ViewChild(MdPaginator) paginator: MdPaginator;
 
     constructor(){}
 
     ngOnInit(){
-    	this.data = data;
-        this.dataSource = new TableService(this.paginator, this.data);
+        //Nota: si marca error es por que ya existe la base de datos y no se le puede agregar una nueva tabla
+        //para solucionarlo borra la base evomatik
+        this.tabla=new CIndexedDB("Evomatik","PersonaDelito"); 
+        this.tabla.list().then(lista=>{
+            this.dataSource = new TableService(this.paginator, lista);
+        });
+        this.dataSource = new TableService(this.paginator, data);
     }
 
 }
 
-export class Persona {
-	id          : number;
-    tipo        : string;
-    nombre      : string;
-    razonSocial : string;
-    alias       : string;
- }
-
- const data: Persona[] = [
-	{id:1,	tipo: 'Persona física', nombre: 'Marco Guzman', razonSocial:'', alias:''}
+ const data = [
+    {id:1,  tipo: 'Persona física', nombre: 'Marco Guzman', razonSocial:'', alias:''}
 ];
