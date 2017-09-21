72b2686dab80ac2ecb80de2f1f731bacfa62f397
Roides Javier Cruz Lara <r.c.lara@evomatik.com>
2017-09-21 17:50:02 -0500

    WIP-Pantalla  Crear Usuarios

--- src/app/components/app/usuario/create/create.component.ts
+++ src/app/components/app/usuario/create/create.component.ts
@@ -1,48 +1,78 @@
-import { Component, OnInit } from '@angular/core';
-import { MRadioButton } from '@partials/form/radiobutton/radiobutton.component'
-
-
+import {Component} from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { MiservicioService,MDato } from '@services/miservicio.service';
-
-
-
+import { MOption } from '@partials/form/select2/select2.component'
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Usuario } from '@models/usuario';
 
 @Component({
-  templateUrl: './create.component.html'
+  selector: 'arma-create',
+  templateUrl: 'create.component.html',
 })
 
+export class UsuarioCreateComponent{
 
-export class UsuarioCreateComponent implements OnInit{
-  titulo: String = "Crear nuevo";
-  dato : MDato;
-  datosR: MRadioButton[] = [
-    {
-      "label" : "Radio 1",
-      "value" : "valor"
-    },
-    {
-      "label" : "Radio 2",
-      "value" : "valor2"
+    public form  : FormGroup;
+    public model : Usuario;
+    public titulo: string;
+    private sub: any;
+
+    public sexos : MOption[]=[ 
+        {value:"Masculino", label:"Masculino"},
+        {value:"Femenino", label:"Femenino"}
+       ];
+    public agencias: MOption[]=[ 
+        {value:"Agencia 1", label:"Agencia 1"},
+        {value:"Agencia 2", label:"Agencia 2"},
+        {value:"Agencia 3", label:"Agencia 3"},
+        {value:"Agencia 4", label:"Agencia 4"}
+       ];
+    public cargos : MOption[]=[ 
+        {value:"Cargo 1", label:"Cargo 1"},
+        {value:"Cargo 2", label:"Cargo 2"}
+       ];    
+    public permisos: MOption[]=[ 
+        {value:"Permiso 1", label:"Permiso 1"},
+        {value:"Permiso 2", label:"Permiso 2"},
+        {value:"Permiso 3", label:"Permiso 3"}
+       ];
+    public isEditForm:boolean;
+    constructor(private _fbuilder: FormBuilder,private route: ActivatedRoute) { }
+    ngOnInit(){
+        this.model = new Usuario();
+        this.form  = new FormGroup({
+            'usuario'            : new FormControl(this.model.usuario, [Validators.required,]),
+            'nombre'             : new FormControl(this.model.nombre, [Validators.required,]),
+            'apellidoPaterno'    : new FormControl(this.model.apellidoPaterno, [Validators.required,]),
+            'apellidoMaterno'    : new FormControl(this.model.apellidoMaterno, [Validators.required,]),
+            'numeroContacto'     : new FormControl(this.model.numeroContacto),
+            'sexo'               : new FormControl(this.model.sexo),
+            'distrito'           : new FormControl(this.model.distrito),
+            'fiscalia'           : new FormControl(this.model.fiscalia),
+            'agenciaAdscripcion' : new FormControl(this.model.agenciaAdscripcion),
+            'turno'              : new FormControl(this.model.turno),
+            'email'              : new FormControl(this.model.email),
+            'numeroGafete'       : new FormControl(this.model.numeroGafete),
+            'cargo'              : new FormControl(this.model.cargo),
+            'permiso'            : new FormControl(this.model.permiso, [Validators.required,]),
+            'inhabilitado'       : new FormControl(this.model.inhabilitado) });
+     let id; 
+      this.sub = this.route.params.subscribe(params => {
+      id = +params['id'];
+      this.isEditForm= id ? true:false ;
+      this.titulo = this.isEditForm ? 'Editar usuario' : 'Crear usuario';
+
+      });
+      // traer el id desde el servicio
+      console.log('-> ID: ', id);
+
     }
-  ];
 
-  constructor(
-    private servicio: MiservicioService,
-    private route: ActivatedRoute,
-    private router: Router) {}
-  
-  
-  ngOnInit(){
-   
-    this.dato = new MDato();
-    this.dato.nombre="yolo";
-    
-  }
+    public save(valid : any, model : any):void{
+        console.log('Usuario@save()');
+    }
+    public edit(valid : any, model : any):void{
+        console.log('Usuario@edit()');
+    }
 
-  save(data: MDato){
-    console.log(data);
-      this.servicio.post(data);
-    this.router.navigate(['/usuarios']);
-  }
+
 }
---
+++ src/app/components/app/usuario/usuarios.component.ts
@@ -0,0 +1,35 @@
+import { Component, ViewChild } from '@angular/core';
+import { MdPaginator } from '@angular/material';
+import { TableService} from '@utils/table/table.service';
+import { Usuario } from '@models/usuario';
+
+
+@Component({
+    templateUrl:'./usuarios.component.html',
+    selector:'usuarios'
+})
+
+export class UsuariosComponent{
+	displayedColumns = ['Usuario', 'Nombre', 'Apellido paterno','Apellido materno','Acciones'];
+	data:Usuario[];
+
+	dataSource: TableService | null;
+	@ViewChild(MdPaginator) paginator: MdPaginator;
+
+	constructor(){}
+
+	ngOnInit() {
+      this.data = [
+    {id:1,usuario:"Usuario",nombre: "Nombre Usuario",apellidoPaterno: "Apellido de Usuario",
+    apellidoMaterno:"Apellido Materno de usuario",numeroContacto:"Data",sexo:"Data",distrito:"Data",
+    fiscalia:"Data",agenciaAdscripcion:"Data",turno:"Data",email:"Data",numeroGafete:"Data",cargo:"Data",permiso:"Data",inhabilitado:false
+    }
+   ];
+    	this.dataSource = new TableService(this.paginator, this.data);
+
+    	console.log('-> Data Source', this.dataSource);
+  	}
+ delete(id) {
+    console.log('Usuario@delete()');
+  }
+}
