51c81b82c0bb0c2046885e100923b798fbbbb8c3
Ulises Hernandez <m.g.hernandez@evomatik.com>
2017-09-25 12:10:39 -0500

    Add - Saving alert

--- src/app/directives/saving.directive.ts
+++ src/app/directives/saving.directive.ts
@@ -1,24 +1,40 @@
 import { Directive, ElementRef, HostListener, Input, Output, EventEmitter } from '@angular/core';
 import { GlobalService } from "@services/global.service";
+import { ConfirmationService } from '@jaspero/ng2-confirmations';
+import { ResolveEmit,ConfirmSettings} from '@utils/alert/alert.service';
 
 @Directive({ selector: '[_saving]' })
 export class SavingDirective{
 	public el         : ElementRef;
 	public saving     : boolean = false;
 	public savingText : string  = '';
+	public settings:ConfirmSettings={
+	    overlayClickToClose: false, // Default: true
+	    showCloseButton: true, // Default: true
+	    confirmText: "Continuar", // Default: 'Yes'
+	    declineText: "Cancelar",
+    };
 	
 	@Output('saveFn') saveFn: EventEmitter<any> = new EventEmitter();
 
-    constructor(_el: ElementRef, public globalService : GlobalService) {
+    constructor(_el: ElementRef, public globalService : GlobalService, private _confirmation: ConfirmationService) {
     	this.el =  _el;
     }
 
 	@HostListener('click') save() {
 		console.log('SavingDirective@save()');
-		this.prepareSave(true);
-		this.saveFn.emit();
-		this.prepareSave(false);
-		this.globalService.openSnackBar('Registro guardado con éxito');
+		this._confirmation.create('','¿Estás seguro de guardar la información?',this.settings)
+         .subscribe(
+         	(ans: ResolveEmit) => {
+         		// console.log(ans);
+         		if(ans.resolved){
+	 				this.prepareSave(true);
+					this.saveFn.emit();
+					this.prepareSave(false);
+					this.globalService.openSnackBar('Registro guardado con éxito');
+				}
+         	}
+		);
 	}
 
 	private getSaveingText(_saving : boolean): string{
