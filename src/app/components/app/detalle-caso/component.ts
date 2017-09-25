bcf84e3cd26619ac1b6ecd1a9bdaf39028ded1ce
Ulises Hernandez <m.g.hernandez@evomatik.com>
2017-09-25 09:31:30 -0500

    Revert "Add - Redirección a noticia de hechos"
    This reverts commit 6a19503816376c53e1fc95dc22cc3dc6124ef7b7.

--- src/app/components/app/detalle-caso/component.ts
+++ src/app/components/app/detalle-caso/component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit} from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
 	templateUrl:'./component.html',
@@ -7,19 +6,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class DetalleCasoComponent implements OnInit{
-	
-	id: number = null;
-
-	route: ActivatedRoute;
-
-	constructor(_route: ActivatedRoute){
-		this.route = _route;
-	} 
-
 	ngOnInit(){
-		this.route.params.subscribe(params => {
-	    	if(params['id'])
-				this.id = +params['id'];
-	    });
+
 	}
 }
