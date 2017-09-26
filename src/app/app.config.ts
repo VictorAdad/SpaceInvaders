e5c9b351eea00ce689db5effa793988f5f1d121c
Ulises Hernandez <m.g.hernandez@evomatik.com>
2017-09-26 16:53:27 -0500

    Change - Guardar casos

---
+++ src/app/app.config.ts
@@ -0,0 +1,5 @@
+export var _config = {
+	api : {
+		host : 'http://10.0.2.80:9000'
+	}
+}
--- src/app/components/app/noticiaHecho/datos-generales/component.ts
+++ src/app/components/app/noticiaHecho/datos-generales/component.ts
@@ -2,11 +2,15 @@ import { Component, OnInit, AfterViewInit, Input} from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MdDialog, MD_DIALOG_DATA} from '@angular/material';
+import { Http, Response } from '@angular/http';
 import { Caso } from '@models/caso';
 import { GlobalComponent } from '@components-app/global.component';
 import { CIndexedDB } from '@services/indexedDB';
-import { FormCreateDelitoCasoComponent } from "./formcreate.component"
-
+import { FormCreateDelitoCasoComponent } from "./formcreate.component";
+import { OnLineService } from '@services/onLine.service';
+import { Observable } from 'rxjs';
+import { _config} from '@app/app.config';
+import 'rxjs/add/operator/map'
 
 @Component({
     selector : 'datos-generales',
@@ -19,6 +23,7 @@ export class DatosGeneralesComponent implements OnInit{
     private router : Router;
     private dialog : MdDialog;
     private activeRoute : ActivatedRoute;
+    private onLine : OnLineService;
     @Input()
     public model   : Caso;
 
@@ -26,12 +31,15 @@ export class DatosGeneralesComponent implements OnInit{
         _dialog: MdDialog,
         _db: CIndexedDB,
         _router: Router,
-        _activeRoute: ActivatedRoute
+        _activeRoute: ActivatedRoute,
+        _onLine: OnLineService,
+        private http: Http
     ) { 
         this.db = _db;
         this.router = _router;
         this.activeRoute = _activeRoute;
         this.dialog = _dialog;
+        this.onLine = _onLine;
     }
 
     ngOnInit(){
@@ -43,16 +51,17 @@ export class DatosGeneralesComponent implements OnInit{
         this.activeRoute.params.subscribe(params => {
             if(this.hasId){
                 this.id = +params['id'];
-                console.log('GET ID: ', this.id );
                 if (!isNaN(this.id)){
-                    this.db.get("casos", this.id).then(object => {
-                        this.model =  object as Caso;
-                        this.form.patchValue({
-                            'titulo'   : this.model.titulo,
-                            'sintesis' : this.model.sintesis,
-                            'delito'   : this.model.delito
+                    if(this.onLine.onLine){
+                        this.find(this.id).subscribe((response) => {
+                            this.form.patchValue(response);
                         });
-                    });
+                    }else{
+                        this.db.get("casos", this.id).then(object => {
+                            this.model = object as Caso;
+                            this.form.patchValue(this.model);
+                        });
+                    }
                 }
                 
             }
@@ -74,19 +83,26 @@ export class DatosGeneralesComponent implements OnInit{
     }
 
     public save(_valid : any, _model : any):void{
-        _model.personas=[];
-        _model.delitos=[];
-        _model.lugares=[];
-        _model.vehiculos=[];
-        _model.armas=[];
-        _model.documentos=[];
-        _model.titulares=[];
-        _model.relaciones=[];
-        console.log('-> Caso@save()', _model);
-        _model.created = new Date();
-        this.db.add('casos', _model).then(object => {
-            this.router.navigate(['/caso/'+object['id']+'/noticia-hecho' ]);
-        });
+        if(this.onLine.onLine){
+            this.post(_model).subscribe((response) => {
+                console.log('Caso guardado en la BD', response);
+                this.router.navigate(['/caso/'+response['id']+'/noticia-hecho' ]);
+            });
+        }else{
+            _model.personas=[];
+            _model.delitos=[];
+            _model.lugares=[];
+            _model.vehiculos=[];
+            _model.armas=[];
+            _model.documentos=[];
+            _model.titulares=[];
+            _model.relaciones=[];
+            console.log('-> Caso@save()', _model);
+            _model.created = new Date();
+            this.db.add('casos', _model).then(object => {
+                this.router.navigate(['/caso/'+object['id']+'/noticia-hecho' ]);
+            });
+        }
     }
 
     public edit(_valid : any, _model : any):void{
@@ -111,8 +127,14 @@ export class DatosGeneralesComponent implements OnInit{
         return hasId;
     }
 
-    public agregarDelito(){
-        console.log('-> Delito agregado');
+    private post(_model: Caso): Observable<Caso>{
+        return this.http.post(_config.api.host+'/v1/base/casos', _model)
+            .map((response: Response) => response.json());
+    }
+
+    private find(_id: number): Observable<Caso>{
+        return this.http.get(_config.api.host+'/v1/base/casos/'+_id)
+            .map((response: Response) => response.json());
     }
 
 }
