289c793b899dd5bda536acf1fbccddaf9a943db3
Ulises Hernandez <m.g.hernandez@evomatik.com>
2017-09-26 17:37:09 -0500

    Add - Home, list de casos

--- src/app/components/app/home/home.component.ts
+++ src/app/components/app/home/home.component.ts
@@ -1,7 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import { ActivatedRoute }    from '@angular/router';
+import { Http, Response } from '@angular/http';
 import { CIndexedDB } from '@services/indexedDB';
+import { OnLineService } from '@services/onLine.service';
 import { Caso } from '@models/caso';
+import { Observable } from 'rxjs';
+import { _config} from '@app/app.config';
+import 'rxjs/add/operator/map'
 
 @Component({
     templateUrl: './home.component.html',
@@ -10,21 +15,41 @@ import { Caso } from '@models/caso';
 export class HomeComponent implements OnInit {
 
     private db: CIndexedDB;
+    private onLine: OnLineService;
+    private http: Http;
     casos: Caso[] = [];
     
 
-    constructor(private route: ActivatedRoute, private _db: CIndexedDB) {
-        this.db = _db;
+    constructor(
+        private route: ActivatedRoute,
+        private _db: CIndexedDB,
+        private _onLine: OnLineService,
+        private _http: Http
+        ) {
+        this.db     = _db;
+        this.onLine = _onLine;
+        this.http   = _http;
     }
 
     ngOnInit(){
-        this.db.list('casos').then(list => {
-            for(let object in list){
-                let caso = new Caso();
-                Object.assign(caso, list[object]);
-                console.log('-> Caso', caso.created);
-                this.casos.push(caso);
-            }
-        });
+        if(this.onLine.onLine){
+            this.list().subscribe((response) => {
+                this.casos = response;
+            });
+        }else{
+            this.db.list('casos').then(list => {
+                for(let object in list){
+                    let caso = new Caso();
+                    Object.assign(caso, list[object]);
+                    console.log('-> Caso', caso.created);
+                    this.casos.push(caso);
+                }
+            });
+        }
+    }
+
+    private list(): Observable<Caso[]>{
+        return this.http.get(_config.api.host+'/v1/base/casos')
+            .map((response: Response) => response.json());
     }
 }
