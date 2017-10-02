caf0b8d93d6e6fdb5f8ed0db2873db9b14298134
Pedro Morales <pedro@Pedros-MacBook-Air.local>
2017-10-02 13:03:52 -0500

    ADD - modelo tiporelacionpersona

---
+++ src/app/models/tipoRelacionPersona.ts
@@ -0,0 +1,13 @@
+import { Caso } from '@media/caso';
+import { Persona } from '@media/persona';
+import { PersonaCaso } from '@media/personaCaso';
+
+export class TipoRelacionPersona {
+	
+	caso: Caso = new Caso();
+	persona: Persona = new Persona();
+	personaCaso: PersonaCaso = new PersonaCaso();
+	detalleDelito: 
+
+
+}
