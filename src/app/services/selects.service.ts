7c461829d8e2d798e0a2681a5f26ec5a869e68cd
Ulises Hernandez <m.g.hernandez@evomatik.com>
2017-10-09 14:21:35 -0500

    Change - Rutas del catalogo de personas

--- src/app/services/selects.service.ts
+++ src/app/services/selects.service.ts
@@ -45,55 +45,55 @@ export class SelectsService {
     }   
 
     public getSexo(){
-        this.http.get('/v1/catalogos/sexo/options').subscribe((response) => {
+        this.http.get('/v1/catalogos/persona/sexo/options').subscribe((response) => {
             this.sexo = this.constructOptions(response);
         });
     }    
 
     public getEscolaridad(){
-        this.http.get('/v1/catalogos/escolaridad/options').subscribe((response) => {
+        this.http.get('/v1/catalogos/persona/escolaridad/options').subscribe((response) => {
             this.escolaridad = this.constructOptions(response);
         });
     }
 
     public getOcupacion(){
-        this.http.get('/v1/catalogos/ocupacion/options').subscribe((response) => {
+        this.http.get('/v1/catalogos/persona/ocupacion/options').subscribe((response) => {
             this.ocupacion = this.constructOptions(response);
         });
     }
 
     public getEstadoCivil(){
-        this.http.get('/v1/catalogos/estado-civil/options').subscribe((response) => {
+        this.http.get('/v1/catalogos/persona/estado-civil/options').subscribe((response) => {
             this.estadoCivil = this.constructOptions(response);
         });
     }
 
     public getIdioma(){
-        this.http.get('/v1/catalogos/idioma/options').subscribe((response) => {
+        this.http.get('/v1/catalogos/persona/idioma/options').subscribe((response) => {
             this.idioma = this.constructOptions(response);
         });
     }
 
     public getGrupoEtnico(){
-        this.http.get('/v1/catalogos/grupo-etnico/options').subscribe((response) => {
+        this.http.get('/v1/catalogos/persona/grupo-etnico/options').subscribe((response) => {
             this.grupoEtnico = this.constructOptions(response);
         });
     }
 
     public getInterprete(){
-        this.http.get('/v1/catalogos/interprete/options').subscribe((response) => {
+        this.http.get('/v1/catalogos/persona/interprete/options').subscribe((response) => {
             this.interprete = this.constructOptions(response);
         });
     }
 
     public getAdiccion(){
-        this.http.get('/v1/catalogos/adiccion/options').subscribe((response) => {
+        this.http.get('/v1/catalogos/persona/adiccion/options').subscribe((response) => {
             this.adiccion = this.constructOptions(response);
         });
     }
 
     public getNacionalidad(){
-        this.http.get('/v1/catalogos/nacionalidad/options').subscribe((response) => {
+        this.http.get('/v1/catalogos/persona/nacionalidad/options').subscribe((response) => {
             this.nacionalidad = this.constructOptions(response);
         });
     }
