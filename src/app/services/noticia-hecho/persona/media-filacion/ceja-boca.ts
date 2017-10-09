cdd55320644a220cee210c7e1b16662abe5dead7
Pedro Morales <pedro@Pedros-MacBook-Air.local>
2017-10-09 11:17:25 -0500

    add - servicios

---
+++ src/app/services/noticia-hecho/persona/media-filacion/ceja-boca.ts
@@ -0,0 +1,39 @@
+import { HttpService } from '@services/http.service';
+import { MatrizGlobal } from '../../matriz-global';
+
+export class MatrizCejaBoca extends MatrizGlobal{
+
+    public direccionCeja           = [];
+    public implantacionCeja        = [];
+    public formaCeja               = [];
+    public tamanioCeja             = [];
+    public tamanioBoca             = [];
+    public comisurasBoca           = [];
+
+    constructor(
+        private http: HttpService
+        ) {
+        super(http);
+        this.selected = new CejaBoca();
+        this.getMatriz('/v1/catalogos/media-filacion/CejaBoca');
+    }
+
+    public validate(_object: any, _selected: any): boolean{
+        return (
+            _object.direccionCeja === _selected.direccionCeja 
+            && _object.implantacionCeja === _selected.implantacionCeja
+            && _object.formaCeja === _selected.formaCeja
+            && _object.tamanioCeja === _selected.tamanioCeja
+            && _object.tamanioBoca === _selected.tamanioBoca
+            && _object.comisurasBoca === _selected.comisurasBoca
+    }
+}
+
+export class CejaBoca {
+    public direccionCeja: string = '';
+    public implantacionCeja: string = '';
+    public formaCeja: string = '';
+    public tamanioCeja: string = '';
+    public tamanioBoca: string = '';
+    public comisurasBoca: string = '';
+}
