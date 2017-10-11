d9047531eedd97c1a7e57317e2127b7dce986e08
Adán <markesz2181@gmail.com>
2017-10-11 11:01:04 -0500

    Fix - campos de Acuerdo general

--- src/app/components/app/solicitud-preliminar/acuerdo-general/create/component.ts
+++ src/app/components/app/solicitud-preliminar/acuerdo-general/create/component.ts
@@ -1,48 +1,48 @@
 import { Component, ViewChild } from '@angular/core';
 import { MdPaginator } from '@angular/material';
-import { TableService} from '@utils/table/table.service';
+import { TableService } from '@utils/table/table.service';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { AcuerdoGeneral } from '@models/solicitud-preliminar/acuerdoGeneral';
-import { OnLineService} from '@services/onLine.service';
-import { HttpService} from '@services/http.service';
+import { OnLineService } from '@services/onLine.service';
+import { HttpService } from '@services/http.service';
 import { SolicitudPreliminarGlobal } from '../../global';
-import { _config} from '@app/app.config';
+import { _config } from '@app/app.config';
 import { CIndexedDB } from '@services/indexedDB';
 
 @Component({
-    templateUrl:'./component.html',
+    templateUrl: './component.html',
 })
 export class AcuerdoGeneralCreateComponent {
-	public casoId: number = null;
+    public casoId: number = null;
     public breadcrumb = [];
 
 
-	constructor(private route: ActivatedRoute){}
+    constructor(private route: ActivatedRoute) { }
 
-	ngOnInit() {
-    	this.route.params.subscribe(params => {
-            if(params['casoId']){
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            if (params['casoId']) {
                 this.casoId = +params['casoId'];
-                this.breadcrumb.push({path:`/caso/${this.casoId}/detalle`,label:"Detalle del caso"})
+                this.breadcrumb.push({ path: `/caso/${this.casoId}/detalle`, label: "Detalle del caso" })
 
             }
 
         });
-  	}
+    }
 
 }
 
 @Component({
-	selector: 'solicitud-acuerdo-general',
-    templateUrl:'./solicitud.component.html',
+    selector: 'solicitud-acuerdo-general',
+    templateUrl: './solicitud.component.html',
 })
 export class SolicitudAcuerdoGeneralComponent extends SolicitudPreliminarGlobal {
-    public apiUrl="/v1/base/solicitudes-pre-acuerdos";
+    public apiUrl = "/v1/base/solicitudes-pre-acuerdos";
     public casoId: number = null;
     public id: number = null;
-    public form  : FormGroup;
-    public model : AcuerdoGeneral;
+    public form: FormGroup;
+    public model: AcuerdoGeneral;
     dataSource: TableService | null;
     @ViewChild(MdPaginator) paginator: MdPaginator;
 
@@ -52,100 +52,100 @@ export class SolicitudAcuerdoGeneralComponent extends SolicitudPreliminarGlobal 
         private onLine: OnLineService,
         private http: HttpService,
         private router: Router,
-        private db:CIndexedDB
-        ) { super();}
+        private db: CIndexedDB
+    ) { super(); }
 
 
-    ngOnInit(){
+    ngOnInit() {
         this.model = new AcuerdoGeneral();
 
-        this.form  = new FormGroup({
-            'fudamentoLegal' : new FormControl(this.model.fudamentoLegal),
+        this.form = new FormGroup({
+            'fudamentoLegal': new FormControl(this.model.fudamentoLegal),
             'contenidoAcuerdo': new FormControl(this.model.contenidoAcuerdo),
-            'finalidad'       : new FormControl(this.model.finalidad),
-            'apercibimientos' : new FormControl(this.model.apercibimientos),
-            'plazo'           : new FormControl(this.model.plazo),
-            'solicita'        : new FormControl(this.model.solicita),
-            'observaciones'   : new FormControl(this.model.observaciones),
-          });
+            'finalidad': new FormControl(this.model.finalidad),
+            'apercibimiento': new FormControl(this.model.apercibimiento),
+            'plazo': new FormControl(this.model.plazo),
+            'senialar': new FormControl(this.model.senialar),
+            'observaciones': new FormControl(this.model.observaciones),
+        });
 
         this.route.params.subscribe(params => {
-            if(params['casoId'])
+            if (params['casoId'])
                 this.casoId = +params['casoId'];
-            if(params['id']){
+            if (params['id']) {
                 this.id = +params['id'];
-                this.http.get(this.apiUrl+'/'+this.id).subscribe(response =>{
-                        this.fillForm(response);
-                    });
+                this.http.get(this.apiUrl + '/' + this.id).subscribe(response => {
+                    this.fillForm(response);
+                });
             }
         });
     }
 
-    public save(valid : any, _model : any):void{
+    public save(valid: any, _model: any): void {
 
-            Object.assign(this.model, _model);
-            this.model.caso.id = this.casoId;
-            console.log('-> AcuerdoGeneral@save()', this.model);
-            this.http.post(this.apiUrl, this.model).subscribe(
+        Object.assign(this.model, _model);
+        this.model.caso.id = this.casoId;
+        console.log('-> AcuerdoGeneral@save()', this.model);
+        this.http.post(this.apiUrl, this.model).subscribe(
 
-                (response) => {
-                    console.log(response);
-                    console.log('here')
-                  if(this.casoId){
-                    this.router.navigate(['/caso/'+this.casoId+'/acuerdo-general' ]);      
-                  }
-                  else {
-                    this.router.navigate(['/acuerdos-generales' ]);      
-                  }
-                },
-                (error) => {
-                    console.error('Error', error);
+            (response) => {
+                console.log(response);
+                console.log('here')
+                if (this.casoId) {
+                    this.router.navigate(['/caso/' + this.casoId + '/acuerdo-general']);
                 }
-            );
+                else {
+                    this.router.navigate(['/acuerdos-generales']);
+                }
+            },
+            (error) => {
+                console.error('Error', error);
+            }
+        );
 
     }
 
-    public edit(_valid : any, _model : any):void{
+    public edit(_valid: any, _model: any): void {
         console.log('-> AcuerdoGeneral@edit()', _model);
-            this.http.put(this.apiUrl+'/'+this.id, _model).subscribe((response) => {
-                console.log('-> Registro acutualizado', response);
-                this.router.navigate(['/caso/'+this.casoId+'/acuerdo-general' ]); 
-            });
-     }
+        this.http.put(this.apiUrl + '/' + this.id, _model).subscribe((response) => {
+            console.log('-> Registro acutualizado', response);
+            this.router.navigate(['/caso/' + this.casoId + '/acuerdo-general']);
+        });
+    }
 
-    public fillForm(_data){
+    public fillForm(_data) {
         this.form.patchValue(_data);
         console.log(_data);
     }
-	
+
 }
 
 @Component({
-	selector: 'documento-acuerdo-general',
-    templateUrl:'./documento.component.html',
+    selector: 'documento-acuerdo-general',
+    templateUrl: './documento.component.html',
 })
 export class DocumentoAcuerdoGeneralComponent {
 
-	displayedColumns = ['nombre', 'procedimiento', 'fechaCreacion'];
-	data: DocumentoAcuerdoGeneral[] = [
-		{id : 1, nombre: 'Entrevista.pdf',  	procedimiento: 'N/A', 		fechaCreacion:'07/09/2017'},
-		{id : 2, nombre: 'Nota.pdf',        	procedimiento: 'N/A', 		fechaCreacion:'07/09/2017'},
-		{id : 3, nombre: 'Fase.png',        	procedimiento: 'N/A', 		fechaCreacion:'07/09/2017'},
-		{id : 4, nombre: 'Entrevista1.pdf',  	procedimiento: 'N/A',     	fechaCreacion:'07/09/2017'},
-		{id : 5, nombre: 'Fase1.png',        	procedimiento: 'N/A',     	fechaCreacion:'07/09/2017'},
-	];
+    displayedColumns = ['nombre', 'procedimiento', 'fechaCreacion'];
+    data: DocumentoAcuerdoGeneral[] = [
+        { id: 1, nombre: 'Entrevista.pdf', procedimiento: 'N/A', fechaCreacion: '07/09/2017' },
+        { id: 2, nombre: 'Nota.pdf', procedimiento: 'N/A', fechaCreacion: '07/09/2017' },
+        { id: 3, nombre: 'Fase.png', procedimiento: 'N/A', fechaCreacion: '07/09/2017' },
+        { id: 4, nombre: 'Entrevista1.pdf', procedimiento: 'N/A', fechaCreacion: '07/09/2017' },
+        { id: 5, nombre: 'Fase1.png', procedimiento: 'N/A', fechaCreacion: '07/09/2017' },
+    ];
 
-	dataSource: TableService | null;
-	@ViewChild(MdPaginator) paginator: MdPaginator;
+    dataSource: TableService | null;
+    @ViewChild(MdPaginator) paginator: MdPaginator;
 
-	ngOnInit() {
-    	this.dataSource = new TableService(this.paginator, this.data);
-  	}
+    ngOnInit() {
+        this.dataSource = new TableService(this.paginator, this.data);
+    }
 }
 
 export interface DocumentoAcuerdoGeneral {
-	id:number
-	nombre: string;
-	procedimiento: string;
-	fechaCreacion: string;
+    id: number
+    nombre: string;
+    procedimiento: string;
+    fechaCreacion: string;
 }
