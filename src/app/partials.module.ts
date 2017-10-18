cd71f207e816387393810fd092e58ece1078e074
Humberto Sánchez <h.s.martinez@evomatik.com>
2017-10-18 10:10:21 -0500

    add - nuevo componente "time" y se agrega guardado y edición de fecha y hora en inspección

--- src/app/partials.module.ts
+++ src/app/partials.module.ts
@@ -5,9 +5,9 @@ import { DateComponent } from '@partials/form/date/date.component';
 import { TextareaComponent } from '@partials/form/textarea/textarea.component';
 import { RadioButtonComponent} from '@partials/form/radiobutton/radiobutton.component';
 import { Select2Component } from '@partials/form/select2/select2.component';
-import { TokenInputComponent } from '@partials/form/tokeninput/tokeninput.component'
-import { BreadcrumbComponent } from '@partials/breadcrumb/breadcrumb.component'
-
+import { TokenInputComponent } from '@partials/form/tokeninput/tokeninput.component';
+import { BreadcrumbComponent } from '@partials/breadcrumb/breadcrumb.component';
+import { TimeComponent } from '@partials/form/time/time.component';
 
 
 @NgModule()
@@ -21,5 +21,6 @@ export const partialsComponents = [
 	RadioButtonComponent,
 	TokenInputComponent,
 	Select2Component,
-	BreadcrumbComponent
+	BreadcrumbComponent,
+	TimeComponent
 ];
