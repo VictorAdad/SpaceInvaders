ed3f39e060536c6e85c3119b7e903129a52ddcd1
Ulises Hernandez <m.g.hernandez@evomatik.com>
2017-10-27 18:35:27 -0500

    Add - NIC component

---
+++ src/app/components/globals/partials/caso/component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CasoService } from '@services/caso.service';
+import { HttpService} from '@services/http.service';
+
+@Component({
+	selector    : 'caso-nic',
+  	templateUrl : './component.html'
+})
+export class CasoNicComponent implements OnInit{
+
+	@Input()
+	public id: number;
+
+	public nic: any;
+
+	public nuc: any;
+
+	
+	constructor(
+		public route: ActivatedRoute,
+		public http: HttpService
+		){
+	}
+
+	ngOnInit(){
+		this.getCaso(this.id);
+	}
+
+	public getCaso(_id){
+        console.log('CasoService@getCaso')
+		this.http.get('/v1/base/casos/'+this.id).subscribe(
+			response => {
+				console.log('Caso', response);
+            	this.nic = response.nic
+        	}
+        );
+	}
+}
+
+
+
