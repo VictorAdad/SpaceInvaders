0fdf789c76ca85c440db856a2b6e8161ea29ded5
UlisesHllSk <divinecurse_hs@hotmail.com>
2017-11-02 22:47:05 -0600

    Fix - Video tutoriales

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -28,27 +28,27 @@ export var _config = {
 		videos: [
             {
                 'title': 'Noticia de hechos',
-                'src': 'https://drive.google.com/uc?export=download&confirm=gXPF&id=0B6-s_1GgDVRAdmJySkJfUjdVZ1E',
+                'src': 'https://dl.dropboxusercontent.com/s/9jkcozjky9ik81a/SIGI%20%281%29%20-%20Noticia%20de%20Hechos.mp4',
             },
             {
                 'title': 'Predenuncia',
-                'src': 'https://drive.google.com/uc?export=download&confirm=gXPF&id=0B6-s_1GgDVRAQ19tS1p0YUJfY2s',
+                'src': 'https://dl.dropboxusercontent.com/s/g65dpfm58qnjlob/SIGI%20%282%29%20-%20Pre%20Denuncia.mp4',
             },
             {
                 'title': 'Entrevistas',
-                'src': 'https://drive.google.com/uc?export=download&confirm=gXPF&id=0B6-s_1GgDVRAcXV0TVBKcVZIaUE',
+                'src': 'https://dl.dropboxusercontent.com/s/z1y7giesqt8h0t6/SIGI%20%283%29%20-%20Entrevistas.mp4',
             },
             {
                 'title': 'Solicitudes preliminares',
-                'src': 'https://drive.google.com/uc?export=download&confirm=gXPF&id=0B6-s_1GgDVRAcVlfeTlhcndycmc',
+                'src': 'https://dl.dropboxusercontent.com/s/j8je6981ii0nz06/SIGI%20%284%29%20-%20Solicitudes%20Preliminares.mp4',
             },
             {
                 'title': 'Acuerdos',
-                'src': 'https://drive.google.com/uc?export=download&confirm=gXPF&id=0B6-s_1GgDVRAR0ZfTGRMbG1xNHM',
+                'src': 'https://dl.dropboxusercontent.com/s/wwdkumbk22gnav1/SIGI%20%285%29%20-%20Acuerdos.mp4',
             },
             {
                 'title': 'Determinaciones',
-                'src': 'https://drive.google.com/uc?export=download&confirm=gXPF&id=0B6-s_1GgDVRATS0yeHE5b1lFN0U',
+                'src': 'https://dl.dropboxusercontent.com/s/4c1qyfen79zbg7b/SIGI%20%286%29%20-%20Determinaciones.mp4',
             }
         ]
 	}
