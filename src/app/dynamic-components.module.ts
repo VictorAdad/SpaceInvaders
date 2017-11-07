1dbb2d11e440ae17bdb0e11db164ccee906c62ce
Saúl Salazar Méndez <s.s.mendez@evomatik.com>
2017-11-07 15:14:00 -0600

    ADD - Progress dialog, dialog para mostrar el avance de descarga de catalogos

--- src/app/dynamic-components.module.ts
+++ src/app/dynamic-components.module.ts
@@ -3,6 +3,7 @@ import { TransferirComponent } from '@components-app/noticiaHecho/titular/compon
 import {FormCreateDelitoComponent} from "@components-app/noticiaHecho/delito/create/formcreate.component"
 import {FormCreateDelitoCasoComponent} from "@components-app/noticiaHecho/datos-generales/formcreate.component"
 import {SolPreDocComponent} from "@components-app/solicitud-preliminar/formatos"
+import {ProgressDialog} from '@components-app/onLine/progressDialog.component';
 
 @NgModule()
 export class DynamicComponentsModule {}
@@ -11,5 +12,6 @@ export const dyanamicComponents = [
     TransferirComponent,
     FormCreateDelitoComponent,
     FormCreateDelitoCasoComponent,
-    SolPreDocComponent
+    SolPreDocComponent,
+    ProgressDialog
 ];
