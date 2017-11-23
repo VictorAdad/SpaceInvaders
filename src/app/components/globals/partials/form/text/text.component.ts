4883b5063d3592f486b0a182f619d462a0e97e8a
Ulises Hernandez <m.g.hernandez@evomatik.com>
2017-11-23 15:20:00 -0600

    Add - Validar RFC y CURP

--- src/app/components/globals/partials/form/text/text.component.ts
+++ src/app/components/globals/partials/form/text/text.component.ts
@@ -52,18 +52,30 @@ export class TextComponent implements OnInit{
 	inputSlice(){
 		if (this.value!=null) {
 			if (this.valRfCurp == true){
-				if (this.value.length == 18 || this.value == null || this.value == "") {
-					this.hintEnd = ""
-					this.group.controls[this.name].setErrors(null);
+				let regex = /^[A-Z]{1}[AEIOU]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[HM]{1}(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}[0-9]{1}$/
+				if (this.value.length == 18 || this.value == null || this.value == ""){
+					if(!regex.test(this.value)){
+						this.hintEnd = "Este CURP no es valido"
+						this.group.controls[this.name].setErrors({'incorrect': true});
+					}else{	
+						this.hintEnd = ""
+						this.group.controls[this.name].setErrors(null);
+					}
 				}else{
 					this.hintEnd = "Este CURP no es valido"
 					this.group.controls[this.name].setErrors({'incorrect': true});	
 				}	
 			}
 			if (this.valRfCurp == false){
+				let regex = /^[A-Z,Ñ,&]{3,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[A-Z|\d]{3}$/
 				if (this.value.length >= 12 || this.value == null || this.value == "") {
-					this.hintEnd = ""
-					this.group.controls[this.name].setErrors(null);
+					if(!regex.test(this.value)){
+						this.hintEnd = "Este RFC no es valido"
+						this.group.controls[this.name].setErrors({'incorrect': true});
+					}else{	
+						this.hintEnd = ""
+						this.group.controls[this.name].setErrors(null);
+					}
 				}else{
 					this.hintEnd = "Este RFC no es valido"
 					this.group.controls[this.name].setErrors({'incorrect': true});	
