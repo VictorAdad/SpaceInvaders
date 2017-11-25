fd95f6d26f7de08c96ab1072b503c71cdbcf9c1d
Ulises Hernandez <m.g.hernandez@evomatik.com>
2017-11-24 18:35:55 -0600

    Add - Validación CURP

--- src/app/components/app/noticiaHecho/global.ts
+++ src/app/components/app/noticiaHecho/global.ts
@@ -9,7 +9,9 @@ export class NoticiaHechoGlobal{
 	public validateForm(form: FormGroup) {
         Object.keys(form.controls).forEach(field => {
             const control = form.get(field);         
-            if (control instanceof FormControl) {         
+            if (control instanceof FormControl) {
+                if(control.status === 'INVALID')
+                    console.log('Validate Control', control);         
                 control.markAsTouched({ onlySelf: true });
             } else if (control instanceof FormGroup) {
                 this.validateForm(control);           
