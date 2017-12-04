0ef34ab4fc2e2c8b1474b23051ffbbd677dd9cf3
Ulises Hernandez <m.g.hernandez@evomatik.com>
2017-12-04 12:38:28 -0600

    Change - Componente de hora #63

--- src/app/components/globals/partials/form/time/time.component.ts
+++ src/app/components/globals/partials/form/time/time.component.ts
@@ -21,11 +21,24 @@ export class TimeComponent implements OnInit{
 	@Input() readonly: string="";
 	@Input() functionChange: Function;
 
+	public max: number = 5
 
-	@Output() valueChange:EventEmitter<string> = new EventEmitter<String>();
+	public backupValue: string;
+
+	public control: FormControl;
+
+	public regex: RegExp = /^([0-1]?[0-9]|2[0-3])(:[0-5][0-9])?$/;
+
+	public regexTime: RegExp = /^[0-9():]*$/;
+
+
+	@Output() valueChange:EventEmitter<string> = new EventEmitter<string>();
 
 	ngOnInit(){
-		// console.log(this);
+		if(this.name != ''){
+			this.control = this.group.get(this.name) as FormControl;
+			this.control.valueChanges.subscribe(this.validate.bind(this));
+		}
 	}
 
 	update(value) {
@@ -33,6 +46,35 @@ export class TimeComponent implements OnInit{
 		if(this.functionChange){
 			this.functionChange(value);
 		}
+	}
+
+	public validate(_val){
+		// console.log('Time ', _val);
+		if(_val != null)
+			if(_val.length > 0){
+				if(_val.length <= this.max){
+					if(this.regexTime.test(_val)){
+						if(!this.regex.test(_val)){
+							this.hintEnd = "Formato invalido";
+							this.control.setErrors({'incorrect': true});
+							// this.control.setValue(this.backupValue);
+						}else{
+							this.hintEnd = "";
+							this.control.setErrors(null);
+						}
+						this.backupValue = _val;
+					}else{
+						this.control.setValue(this.backupValue);	
+					}
+				}else{
+					this.control.setValue(this.backupValue);
+				}
+			}else{
+				this.backupValue = _val;
+				this.hintEnd = "";
+				this.control.setErrors(null);
+			}
+
 	}
 }
 
