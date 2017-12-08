3d8ce25d72d8739de23dac230f6b5556801cb81c
Pedro Morales <pedro@Pedros-MacBook-Air.local>
2017-12-08 17:12:22 -0600

    fix - issue #227

--- src/app/components/app/noticiaHecho/persona/create/options.ts
+++ src/app/components/app/noticiaHecho/persona/create/options.ts
@@ -76,10 +76,20 @@ export class Options {
         }
     }
 
-    public getLocalidadByMunicipio(idColonia: number){
-        this.http.get('/v1/catalogos/localidad/municipio/'+idColonia+'/options').subscribe((response) => {
-            this.localidad = this.constructOptions(response);
-        });
+    public getLocalidadByMunicipio(idMunicipio: number){
+        if (this.onLine.onLine) {
+            this.http.get('/v1/catalogos/localidad/municipio/'+idMunicipio+'/options').subscribe((response) => {
+                this.localidad = this.constructOptions(response);
+            });
+        }else{
+            this.db.searchInNotMatrx("localidad",{municipio:{id:idMunicipio}}).then(response=>{
+                let localidad={};
+                for(let e in response){
+                    localidad[""+response[e].id]=response[e].nombre
+                }
+                this.localidad=this.constructOptions(localidad);
+            });
+        }
     }
 
     public constructOptions(_data:any){
