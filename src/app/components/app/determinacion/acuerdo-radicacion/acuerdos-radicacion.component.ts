441e501e9fdb58b0ad7a2becda76c012bfe4478c
Roides Javier Cruz Lara <r.c.lara@evomatik.com>
2017-12-08 16:58:37 -0600

    Change - Paginador acuerdo radicación

--- src/app/components/app/determinacion/acuerdo-radicacion/acuerdos-radicacion.component.ts
+++ src/app/components/app/determinacion/acuerdo-radicacion/acuerdos-radicacion.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
+import { BasePaginationComponent } from './../../base/pagination/component';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MatPaginator } from '@angular/material';
 import { TableService} from '@utils/table/table.service';
@@ -13,10 +14,9 @@ import { Logger } from "@services/logger.service";
     selector:'acuerdos-radicacion'
 })
 
-export class AcuerdosRadicacionComponent{
+export class AcuerdosRadicacionComponent  extends BasePaginationComponent implements OnInit{
 
    public breadcrumb = [];
-   public apiUrl:string="/v1/base/acuerdos/casos/{id}/page";
    columns = ['Fecha'];
    public dataSource: TableService | null;
    public data: AcuerdoRadicacion[];
@@ -26,39 +26,54 @@ export class AcuerdosRadicacionComponent{
    paginator: MatPaginator;
    public pag: number = 0;
 
-  constructor(private route: ActivatedRoute, private http: HttpService, private onLine: OnLineService, private db:CIndexedDB){}
+  constructor(private route: ActivatedRoute, private http: HttpService, private onLine: OnLineService, private db:CIndexedDB){super();}
   ngOnInit() {
         this.route.params.subscribe(params => {
             if(params['casoId']){
               this.haveCaso=true;
                 this.casoId = +params['casoId'];
-                this.apiUrl = this.apiUrl.replace("{id}", String(this.casoId));
                 this.breadcrumb.push({path:`/caso/${this.casoId}/detalle`,label:"Detalle de caso"});
-                this.page(this.apiUrl);
+                this.page();
             }
 
         });
     }
 
-    public changePage(_e) {
-        this.page(this.apiUrl + '?p=' + _e.pageIndex + '&tr=' + _e.pageSize);
-    }
+    public changePage(_e){
+      console.log('changePage()', _e);
+      this.dataSource = null;
+      this.pageIndex  = _e.pageIndex;
+      this.pageSize   = _e.pageSize;
+      this.page();
+  }
 
-    public page(url: string) {
-        this.data = [];
-        this.http.get(url).subscribe((response) => {
-            //Logger.log('Paginator response', response.data);
+  public filterPage(_event){
+      if(typeof _event == 'string'){
+          this.dataSource = null;
+          this.pageFilter = _event;
+          this.page();
+      }
+  }
 
-            response.data.forEach(object => {
-                this.pag = response.totalCount;
-                //Logger.log("Respuestadelitos", response["data"]);
-                this.data.push(Object.assign(new AcuerdoRadicacion(), object));
-                //response["data"].push(Object.assign(new Caso(), object));
-                this.dataSource = new TableService(this.paginator, this.data);
-            });
-            Logger.log('Datos finales', this.dataSource);
-        });
-    }
+    public page(){
+      this.loadList = true;
+      this.http.get(
+          `/v1/base/acuerdos/casos/${this.casoId}/page?f=${this.pageFilter}&p=${this.pageIndex}&tr=${this.pageSize}`
+      ).subscribe(
+          (response) => {
+              this.data = [];
+              this.loadList = false;
+              response.data.forEach(object => {
+                  this.pag = response.totalCount;
+                  this.data.push(object);
+                  this.dataSource = new TableService(this.paginator, this.data);
+              });
+          },
+          (error) => {
+              this.loadList = false
+          }
+      );
+  }
 }
 
 
