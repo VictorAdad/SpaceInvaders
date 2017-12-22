bef98f317027a8c9c07a9d66bc8b4b647ba693d1
Ulises Hernandez <m.g.hernandez@evomatik.com>
2017-12-22 16:46:47 -0600

    Add  - OAM vars

--- src/app/services/auth/authentication.service.ts
+++ src/app/services/auth/authentication.service.ts
@@ -17,6 +17,7 @@ permanezca conectado entre actualizaciones o sesiones, el comportamiento podría
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Observable } from 'rxjs';
+import { Subject } from 'rxjs/Subject';
 import { _usuarios } from './usuarios';
 import { environment } from '../../../environments/environment';
 import 'rxjs/add/operator/map'
@@ -32,12 +33,14 @@ export class AuthenticationService {
 
     public roles:any;
 
+    public subject = new Subject<any>();
+
     constructor(private http: Http) {
 
         this.headers = new Headers();
         this.headers.append('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
-        this.headers.append('Authorization', 'Basic b2F1dGhzaWdpY2xpZW50OmZjODFmZGFmNjlhYjQ4NjZhMmZjODU3NWMwZGIwYmQ2');
-        this.headers.append('X-OAUTH-IDENTITY-DOMAIN-NAME', 'OAuthSIGIDomain');
+        this.headers.append('Authorization', 'Basic '+environment.oam.tokenApp);
+        this.headers.append('X-OAUTH-IDENTITY-DOMAIN-NAME', environment.oam.domainName);
 
         let session = localStorage.getItem(environment.oam.session);
 
@@ -48,7 +51,6 @@ export class AuthenticationService {
 
             request.subscribe(
                 responseUser => {
-                    console.log('Response User '+responseUser );
                     responseUser['token'] =  usuario.token;
                     this.user = new Usuario(responseUser);
                     localStorage.setItem(environment.oam.session, JSON.stringify(this.user));
@@ -60,12 +62,12 @@ export class AuthenticationService {
 
 
         this.roles = {
-            'callCenter': 'callCenter',
+            'callCenter': 'Call Center',
             'uai': 'uai',
             'express': 'express',
             'mpuai': 'mpuai',
-            'mpi': 'mpi',
-            'admin': 'admin',
+            'mpi': 'M.P.I',
+            'admin': 'Admin',
         }
     }
 
@@ -74,7 +76,6 @@ export class AuthenticationService {
         var requestToken = this.getToken(username, password);
         requestToken.subscribe(
             response => {
-                console.log('-> Response Token', response);
                 let requestUser = this.getUser(response.access_token);
 
                 requestUser.subscribe(responseUser => {
@@ -142,21 +143,17 @@ export class Usuario {
             this.username = _usuario.sub;
             this.roles = this.setRoles(_usuario.Roles);
             this.token = _usuario.token;
-            // this.fiscalia = _usuario.fiscalia;
+            this.fiscalia = _usuario.Municipio;
             this.agencia = _usuario.Municipio;
-            // this.autoridad = _usuario.autoridad;
-            // this.turno = _usuario.turno;
+            this.autoridad = _usuario.Municipio;
+            this.turno = _usuario.Turno;
             // this.distrito = _usuario.distrito;
             this.municipio = _usuario.Municipio;
         }
     }
 
     public setRoles(_roles: string): string[]{
-        let roles: string[] = _roles.split('\\');
-
-        for (let role of _roles) {
-            roles.push(role);
-        }
+        let roles: string[] = _roles.split('\\,');
 
         return roles;
     }
