cfebce7d3ca02ca5faf8485171db345ad10ec5d5
Roides Javier Cruz Lara <r.c.lara@evomatik.com>
2017-12-26 15:56:10 -0600

    Add- Caso Herencia component

--- src/app/components/app/determinacion/facultad-no-investigar/create/create.component.ts
+++ src/app/components/app/determinacion/facultad-no-investigar/create/create.component.ts
@@ -4,7 +4,7 @@ import { MatPaginator } from '@angular/material';
 import { MatDialogRef, MatDialog, MAT_DIALOG_DATA} from '@angular/material';
 import { TableService } from '@utils/table/table.service';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { FacultadNoInvestigar } from '@models/determinacion/facultad-no-investigar';
 import { OnLineService } from '@services/onLine.service';
 import { HttpService } from '@services/http.service';
@@ -73,6 +73,21 @@ export class FacultadNoInvestigarComponent extends DeterminacionGlobal {
     ngOnInit() {
         this.model = new FacultadNoInvestigar();
         this.form = new FormGroup({
+            'lugar': new FormGroup({
+              'id': new FormControl("", []),
+            }),
+            'arma': new FormGroup({
+              'id': new FormControl("", []),
+            }),
+            'vehiculo': new FormGroup({
+              'id': new FormControl("", []),
+            }),
+            'delito': new FormGroup({
+              'id': new FormControl("", []),
+            }),
+            'heredar':  new FormControl("", []),
+            'personas': new FormArray([]),
+
             'observaciones': new FormControl(),
             'sintesisHechos': new FormControl(),
             'datosPrueba': new FormControl(),
