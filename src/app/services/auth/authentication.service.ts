dbaefabbebd66a58939dedc391219460b27d8b16
Ulises Hernandez <m.g.hernandez@evomatik.com>
2017-12-27 13:12:26 -0600

    Add - User class

--- src/app/services/auth/authentication.service.ts
+++ src/app/services/auth/authentication.service.ts
@@ -19,6 +19,7 @@ import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Observable } from 'rxjs';
 import { Subject } from 'rxjs/Subject';
 import { _usuarios } from './usuarios';
+import { Usuario } from './user';
 import { environment } from '../../../environments/environment';
 import 'rxjs/add/operator/map'
 
@@ -120,58 +121,6 @@ export class AuthenticationService {
         else {
             return true;
         }
-    }
-}
-
-export class Usuario {
-
-    public nombreCompleto: string;
-    public username: string;
-    public roles: string[];
-    public token: string;
-    public fiscalia: string;
-    public agencia: string;
-    public turno: string;
-    public autoridad: string;
-    public distrito: string;
-    public municipio: string;
-    
-    constructor(_usuario: any =  null) {
-        if(_usuario != null){
-            console.log('New Usuario()', _usuario);
-            this.nombreCompleto = _usuario.sub;
-            this.username = _usuario.sub;
-            this.roles = this.setRoles(_usuario.Roles);
-            this.token = _usuario.token;
-            this.fiscalia = _usuario.Municipio;
-            this.agencia = _usuario.Municipio;
-            this.autoridad = _usuario.Municipio;
-            this.turno = _usuario.Turno;
-            // this.distrito = _usuario.distrito;
-            this.municipio = _usuario.Municipio;
-        }
-    }
-
-    public setRoles(_roles: string): string[]{
-        let roles: string[] = _roles.split('\\,');
-
-        return roles;
-    }
-
-    public hasRoles(..._roles): boolean{
-        if(this.roles)
-            return _roles.some( role => this.roles.includes(role));
-        else
-            return false;   
-     }
-}
-
-export class Role {
-
-    public nombre: string;
-    
-    constructor(_role: string) {
-        this.nombre = _role;
     }
 }
 
---
+++ src/app/services/auth/user.ts
@@ -0,0 +1,56 @@
+export class Usuario {
+
+    public nombreCompleto: string;
+    public username: string;
+    public roles: string[];
+    public token: string;
+    public fiscalia: string;
+    public fiscaliaCompleto: string;
+    public agencia: string;
+    public agenciaCompleto: string;
+    public turno: string;
+    public autoridad: string;
+    public autoridadCompleto: string;
+    public distrito: string;
+    public distritoCompleto: string;
+    public municipio: string;
+    public municipioCompleto: string;
+    
+    constructor(_usuario: any =  null) {
+        if(_usuario != null){
+            console.log('New Usuario()', _usuario);
+            this.nombreCompleto = _usuario.sub;
+            this.username = _usuario.sub;
+            this.roles = this.setRoles(_usuario.Roles);
+            this.token = _usuario.token;
+            this.fiscalia = _usuario.Municipio;
+            this.agencia = _usuario.Municipio;
+            this.autoridad = _usuario.Municipio;
+            this.turno = _usuario.Turno;
+            // this.distrito = _usuario.distrito;
+            this.municipio = _usuario.Municipio;
+        }
+    }
+
+    public setRoles(_roles: string): string[]{
+        let roles: string[] = _roles.split('\\,');
+
+        return roles;
+    }
+
+    public hasRoles(..._roles): boolean{
+        if(this.roles)
+            return _roles.some( role => this.roles.includes(role));
+        else
+            return false;   
+     }
+}
+
+export class Role {
+
+    public nombre: string;
+    
+    constructor(_role: string) {
+        this.nombre = _role;
+    }
+}
