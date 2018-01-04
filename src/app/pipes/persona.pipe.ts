12acccfd3e0de991a2d147af4353cb2d1eaabaed
Roides Javier Cruz Lara <r.c.lara@evomatik.com>
2018-01-04 15:26:49 -0600

    add- Pipe de Domicilio de persona

--- src/app/pipes/persona.pipe.ts
+++ src/app/pipes/persona.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name : 'personaNombre'})
 export class PersonaNombre implements PipeTransform {
 	transform(element:any){
-		var nombre 
+		var nombre
 
 		if (element.tipoInterviniente.tipo == "Víctima desconocida" || element.tipoInterviniente.tipo == "Imputado desconocido"){
 			if (element.tipoInterviniente.tipo == "Imputado desconocido") {
@@ -44,6 +44,24 @@ export class PersonaNombre implements PipeTransform {
 			nombre = element.persona.nombre+" "+element.persona.paterno+" "+element.persona.materno;
 		}
 
-		return nombre;	
+		return nombre;
+	}
+}
+
+
+@Pipe({name : 'personaDomicilio'})
+export class PersonaDomicilio implements PipeTransform {
+	transform(personaCaso:any,idLocalizacion){
+    let localizacion = personaCaso.persona.localizacionPersona[idLocalizacion];
+		var domicilio="";
+    if(localizacion){
+    let calle=localizacion.calle?localizacion.calle:null;
+    let noExterior=localizacion.noExterior?localizacion.noExterior:null;
+    let municipio=(localizacion.municipio?localizacion.municipio.nombre:(localizacion.municipioOtro?localizacion.municipioOtro:null))
+    let colonia=(localizacion.colonia?localizacion.colonia.nombre:(localizacion.coloniaOtro?localizacion.coloniaOtro:null))
+    let estado=(localizacion.estado?localizacion.estado.nombre:(localizacion.estadoOtro?localizacion.estadoOtro:null))
+    domicilio=(calle?calle+" ":"")+ (noExterior?noExterior+" ":"")+(colonia?colonia+" ":"")+(municipio?municipio+" ":"")+(estado?estado+" ":"")
+    }
+		return domicilio;
 	}
 }
