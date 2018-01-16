5852ec22f8e66ab87f22e7f3591a28e59eeca737
Saúl Salazar Méndez <s.s.mendez@evomatik.com>
2018-01-16 14:40:00 -0600

    CHANGE - se cambio para que cargue todo lo de assets menos el saas

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,6 +1,6 @@
 module.exports = {  
     staticFileGlobs: 
-        [    'dist/**.html',    'dist/**.js',    'dist/**.css',    'dist/assets/images/*',    'dist/assets/icons/*'  ],  
+        [    'dist/**.html',    'dist/**.js',    'dist/**.css',    'dist/assets/**', '!dist/assets/sass/**' ],  
     root: 'dist',  
     stripPrefix: 'dist/',  
     navigateFallback: '/index.html'
