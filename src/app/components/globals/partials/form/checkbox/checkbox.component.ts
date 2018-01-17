c3d4169651eb3eb0d56ae3946844c8ab0053482b
Ulises Hernandez <m.g.hernandez@evomatik.com>
2018-01-17 17:58:29 -0600

    Fix - Herencia, mostrar checkbox de sintesis del caso #325

--- src/app/components/globals/partials/form/checkbox/checkbox.component.ts
+++ src/app/components/globals/partials/form/checkbox/checkbox.component.ts
@@ -2,24 +2,25 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-	selector    : 'checkbox',
-  	templateUrl : './checkbox.component.html'
+    selector    : 'checkbox',
+      templateUrl : './checkbox.component.html'
 })
 export class CheckboxComponent{
-	@Input() label : string;
-	@Input() value : string;
-	@Input() name  : string  = '';
-	@Input() group : FormGroup = new FormGroup({});
+    @Input() label : string;
+    @Input() value : string;
+    @Input() name  : string  = '';
+    @Input() disabled = false;
+    @Input() group : FormGroup = new FormGroup({});
 
-	@Output() valueChange:EventEmitter<string> = new EventEmitter<String>()
+    @Output() valueChange:EventEmitter<string> = new EventEmitter<String>()
 
-	update(value) {
-		this.valueChange.emit(value);
-	}
+    update(value) {
+        this.valueChange.emit(value);
+    }
 
-	changeCheck(e){
-		this.valueChange.emit(e.checked);
-	}
+    changeCheck(e){
+        this.valueChange.emit(e.checked);
+    }
 }
 
 
