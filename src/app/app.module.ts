08dc7ff20d963cbc1fcbe89ccf76540875dee565
Pedro Morales <j.m.rodriguez@evomatik.com>
2018-01-25 09:43:05 -0600

    add - se agrego el anexo 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -82,6 +82,7 @@ import { PrincipalInformeHomologadoCreate} from '@components-app/informe-homolog
 import { Anexo1Component} from '@components-app/informe-homologado/anexo1/anexo1.component';
 import { Anexo2Component} from '@components-app/informe-homologado/anexo2/anexo2.component';
 import { Anexo3Component} from '@components-app/informe-homologado/anexo3/anexo3.component';
+import { Anexo5Component} from '@components-app/informe-homologado/anexo5/anexo5.component';
 
 @NgModule({
   declarations: [
@@ -103,6 +104,7 @@ import { Anexo3Component} from '@components-app/informe-homologado/anexo3/anexo3
     Anexo1Component,
     Anexo2Component,
     Anexo3Component,
+    Anexo5Component,
     EntrevistaComponents,
 
     catalogosComponents,
---
+++ src/app/components/app/informe-homologado/anexo5/anexo5.component.ts
@@ -0,0 +1,37 @@
+import { Component, ViewChild } from '@angular/core';
+import { BasePaginationComponent } from '@components-app/base/pagination/component';
+import { TableService } from '@utils/table/table.service';
+import { MatPaginator } from '@angular/material';
+import { ActivatedRoute } from '@angular/router';
+import { HttpService} from '@services/http.service';
+
+@Component({
+    selector: 'anexo5',
+    templateUrl: './anexo5.component.html'
+})
+
+export class Anexo5Component {
+
+    public breadcrumb = [];
+    columns = ['detenido', 'descripcion', 'cantidad'];
+    dataSource: TableService | null;
+
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+
+    constructor(
+        private route: ActivatedRoute,
+        private http: HttpService){
+        // super();
+    }
+
+    ngOnInit(){
+        var rows = {};
+        rows['detenido'] = "Yair Ruiz" 
+        rows['descripcion'] = "Control de contacto"
+        rows['cantidad'] = "4"
+        
+        this.dataSource = new TableService(this.paginator, [rows]);
+    }
+
+
+}
