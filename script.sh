50b69cee133637261c1e47d4308e60114089614a
adad <adad.v95@gmail.com>
2018-01-31 13:16:41 -0600

    ahora ya funciona el script, segun gocd, pero no veo el archivo txt creado

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # This is a comment!
-touch /home/adad/Documents/space/subfolder/document1.txt
-echo "hola mundo" >> /home/adad/Documents/space/subfolder/document1.txt
+touch document1.txt
+echo "hola mundo" >> document1.txt
 # /var/lib/go-agent/pipelines/space-invaders-1/home/adad/Documents/space
