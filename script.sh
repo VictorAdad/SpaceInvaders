1e2ec219b87bd7e77d3f65993bf5288bf81f6488
adad <adad.v95@gmail.com>
2018-01-31 10:45:46 -0600

    el script cambio un poco

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This is a comment!
-touch docu1.txt
-echo "hola mundo" >> docu1.txt
+touch /home/adad/Documents/space/docu1.txt
+echo "hola mundo" >> /home/adad/Documents/space/docu1.txt
 # /var/lib/go-agent/pipelines/space-invaders-1/home/adad/Documents/space
