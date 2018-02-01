e736917808058a434144f11f11411616824e7835
adad <adad.v95@gmail.com>
2018-02-01 14:00:06 -0600

    added parameters to scp line

--- build-test.sh
+++ build-test.sh
@@ -16,4 +16,4 @@ tar cvzf builds/$BUILD_NAME a.out
 # Upload to DEST/DIR_NAME
 echo "Intentando subir el paquete al servidor (en ~/Documents)"
 #scp builds/$BUILD_NAME $DEST # este es el antiguo metodo, no sirve en GoCD
-sshpass -p "evo100518" scp -r builds/examplefile evomatik@10.0.2.152:/home/evomatik/Downloads/
+sshpass -p "evo100518" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r builds/examplefile evomatik@10.0.2.152:/home/evomatik/Downloads/
