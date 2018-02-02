9050d2ca47512a206ba55ce05830b12a8a99379e
adad <adad.v95@gmail.com>
2018-02-02 09:48:26 -0600

    el script crea ahora index.html

--- build-test.sh
+++ build-test.sh
@@ -2,6 +2,8 @@
 # This is a comment!
 echo "Compilando proyecto en C++ llamado test.cpp."
 g++ test.cpp
+touch index.html
+cout "<b>Esto es un index.</b>" >> index.html
 
 # Set the destination server
 DEST=evomatik@10.0.2.152:/home/evomatik/Downloads
@@ -11,9 +13,9 @@ BUILD_NAME="test-compiled-$(date +'%m%d%y').tgz"
 
 # Create the tarball of the build
 echo "Empaquetando a.out para subirlo al server"
-tar cvzf builds/$BUILD_NAME a.out
+tar cvzf builds/$BUILD_NAME index.html
 
 # Upload to DEST/DIR_NAME
-echo "Intentando subir el paquete al servidor (en ~/Documents)"
+echo "Intentando subir el paquete al servidor (/home/evomatik/Downloads)"
 #scp builds/$BUILD_NAME $DEST # este es el antiguo metodo, no sirve en GoCD
 sshpass -p "evo100518" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r builds/$BUILD_NAME $DEST
